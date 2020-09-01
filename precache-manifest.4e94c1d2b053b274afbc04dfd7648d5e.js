fee29de5e4fab69e8434b020266fe8774742e7ac
Kotfil <Dramen333>
2020-09-01 20:17:43 +0300

    Updates

---
+++ precache-manifest.4e94c1d2b053b274afbc04dfd7648d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5817dbdf42bb029d8e95f59f47055257",
+    "url": "/social-ts-ghpages-test/index.html"
+  },
+  {
+    "revision": "5c407b0804edbd307bbd",
+    "url": "/social-ts-ghpages-test/static/css/main.008a704e.chunk.css"
+  },
+  {
+    "revision": "9929ea90a76eb234dd07",
+    "url": "/social-ts-ghpages-test/static/js/2.d54f904f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-ts-ghpages-test/static/js/2.d54f904f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c407b0804edbd307bbd",
+    "url": "/social-ts-ghpages-test/static/js/main.3f409774.chunk.js"
+  },
+  {
+    "revision": "572adb369ab3290d61af",
+    "url": "/social-ts-ghpages-test/static/js/runtime-main.194f7187.js"
+  }
+]);
