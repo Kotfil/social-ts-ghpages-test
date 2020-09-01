004b5f8b6fcf625514768a766ec84102c6948049
Kotfil <supersavka@icloud.com>
2020-09-01 22:04:02 +0300

    add message tag

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -9,8 +9,12 @@ export type PropsType = {
 const Message = (props: PropsType) => {
 
     return(
+        <>
+            <h1>Message</h1>
+            <hr/>
         <div className={s.message}>{props.messages}</div>
-    )
+        </>
+            )
 };
 
 export default Message
