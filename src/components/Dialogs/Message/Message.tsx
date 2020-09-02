72db660029e18e1aec1fac5431a213a6fb9fd24b
Kotfil <supersavka@icloud.com>
2020-09-02 08:37:56 +0300

    deleted old functional

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -5,17 +5,10 @@ export type PropsType = {
     messages: string
 }
 
-
 const Message = (props: PropsType) => {
 
     return(
-        <>
-            <h1>Message</h1>
-            <hr/>
-            <h2>Random text</h2>
-            <h2>Random text2</h2>
         <div className={s.message}>{props.messages}</div>
-        </>
             )
 };
 
